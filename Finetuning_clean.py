dc9eeccf7319dbf86f78e5911ca4456c5aacf00a
Benjamin Dewey <benjamin.m.dewey@gmail.com>
2018-05-12 17:02:21 -0400

    try using tuned original network

--- Finetuning_clean.py
+++ Finetuning_clean.py
@@ -25,7 +25,7 @@ import os,sys,cv2,random,datetime,time,math
 import argparse
 import numpy as np
 
-from net_s3fd import *
+# from net_s3fd import *
 from s3fd import *
 from bbox import *
 from sklearn.preprocessing import MultiLabelBinarizer
@@ -157,7 +157,7 @@ def train_model(model, criterion, optimizer, num_classes, num_epochs = 100):
 
 
 num_classes = 2
-myModel = s3fd(num_classes)
+myModel = s3fd_original(num_classes)
 
 
 loadedModel = torch.load('s3fd_convert.pth')
