116792597960734c8833e0dd6f5c7ce448de3148
Benjamin Dewey <benjamin.m.dewey@gmail.com>
2018-05-12 15:01:21 -0400

    add gender classification layers and compose the forward pass output

--- s3fd.py
+++ s3fd.py
@@ -42,6 +42,7 @@ class s3fd_original(nn.Module):
         self.fc6     = nn.Conv2d(512, 1024, kernel_size=3, stride=1, padding=3)
         self.fc7     = nn.Conv2d(1024, 1024, kernel_size=1, stride=1, padding=0)
 
+
         self.conv6_1 = nn.Conv2d(1024, 256, kernel_size=1, stride=1, padding=0)
         self.conv6_2 = nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1)
 
@@ -52,18 +53,21 @@ class s3fd_original(nn.Module):
         self.conv4_3_norm = L2Norm(512,scale=8)
         self.conv5_3_norm = L2Norm(512,scale=5)
 
-       
+
         self.conv3_3_norm_mbox_conf = nn.Conv2d(256, 4, kernel_size=3, stride=1, padding=1)
         self.conv3_3_norm_mbox_loc  = nn.Conv2d(256, 4, kernel_size=3, stride=1, padding=1)
         self.conv4_3_norm_mbox_conf = nn.Conv2d(512, 2, kernel_size=3, stride=1, padding=1)
-        self.conv4_3_norm_gender = nn.Conv2d(512, 2, kernel_size=3, stride=1, padding=1)
+        self.conv4_3_norm_gender    = nn.Conv2d(512, 2, kernel_size=3, stride=1, padding=1) # gender layer
         self.conv4_3_norm_mbox_loc  = nn.Conv2d(512, 4, kernel_size=3, stride=1, padding=1)
         self.conv5_3_norm_mbox_conf = nn.Conv2d(512, 2, kernel_size=3, stride=1, padding=1)
+        self.conv5_3_norm_gender    = nn.Conv2d(512, 2, kernel_size=3, stride=1, padding=1) # gender layer
         self.conv5_3_norm_mbox_loc  = nn.Conv2d(512, 4, kernel_size=3, stride=1, padding=1)
 
         self.fc7_mbox_conf     = nn.Conv2d(1024, 2, kernel_size=3, stride=1, padding=1)
+        self.fc7_gender        = nn.Conv2d(1024, 2, kernel_size=3, stride=1, padding=1) # gender layer
         self.fc7_mbox_loc      = nn.Conv2d(1024, 4, kernel_size=3, stride=1, padding=1)
         self.conv6_2_mbox_conf = nn.Conv2d(512, 2, kernel_size=3, stride=1, padding=1)
+        self.conv6_2_gender    = nn.Conv2d(512, 2, kernel_size=3, stride=1, padding=1) # gender layer
         self.conv6_2_mbox_loc  = nn.Conv2d(512, 4, kernel_size=3, stride=1, padding=1)
         self.conv7_2_mbox_conf = nn.Conv2d(256, 2, kernel_size=3, stride=1, padding=1)
         self.conv7_2_mbox_loc  = nn.Conv2d(256, 4, kernel_size=3, stride=1, padding=1)
@@ -84,23 +88,23 @@ class s3fd_original(nn.Module):
 
         print("f3_3")
         print(h.size())
-        
+
         h = F.relu(self.conv4_1(h))
         h = F.relu(self.conv4_2(h))
         h = F.relu(self.conv4_3(h)); f4_3 = h
-        
+
 
         h = F.max_pool2d(h, 2, 2)
         print("f4_3")
         print(h.size())
-        
+
         h = F.relu(self.conv5_1(h))
         h = F.relu(self.conv5_2(h))
         h = F.relu(self.conv5_3(h)); f5_3 = h
         h = F.max_pool2d(h, 2, 2)
         print("f5_3")
         print(h.size())
-        
+
         h = F.relu(self.fc6(h))
         h = F.relu(self.fc7(h));     ffc7 = h
         print("ffc7")
@@ -118,37 +122,25 @@ class s3fd_original(nn.Module):
         f5_3 = self.conv5_3_norm(f5_3)
 
         cls1 = self.conv3_3_norm_mbox_conf(f3_3)
-        
-
-
-        
-        
-        #cls1.backward(grads)
-        
-#         print("cls1")
-#         print(cls1.size())
         reg1 = self.conv3_3_norm_mbox_loc(f3_3)
+
         cls2 = self.conv4_3_norm_mbox_conf(f4_3)
-#         print("cls2")
-#         print(cls2.size())        
         reg2 = self.conv4_3_norm_mbox_loc(f4_3)
         gen2 = self.conv4_3_norm_gender(f4_3)
-        
+
         cls3 = self.conv5_3_norm_mbox_conf(f5_3)
-#         print("cls3")
-#         print(cls3.size())        
         reg3 = self.conv5_3_norm_mbox_loc(f5_3)
+        gen3 = self.conv5_3_norm_gender(f5_3)
+
         cls4 = self.fc7_mbox_conf(ffc7)
-#         print("cls4")
-#         print(cls4.size())        
         reg4 = self.fc7_mbox_loc(ffc7)
+        gen4 = self.fc7_gender(ffc7)
+
         cls5 = self.conv6_2_mbox_conf(f6_2)
-#         print("cls5")
-#         print(cls5.size())        
         reg5 = self.conv6_2_mbox_loc(f6_2)
+        gen5 = self.conv6_2_gender(f6_2)
+
         cls6 = self.conv7_2_mbox_conf(f7_2)
-#         print("cls6")
-#         print(cls6.size())        
         reg6 = self.conv7_2_mbox_loc(f7_2)
 
         #max-out background label
@@ -159,4 +151,4 @@ class s3fd_original(nn.Module):
         print(cls1.size())
         #return [cls1,reg1,cls2,reg2,cls3,reg3,cls4,reg4,cls5,reg5,cls6,reg6]
         #return [cls1,cls2,cls3,cls4,cls5,cls6]
-        return [cls2,gen2]
+        return [cls2,gen2,cls3,gen3,cls4,gen4,cls5,gen5]
