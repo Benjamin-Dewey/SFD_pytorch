6512841b2945d2bae5bea30ad6aa2882d0cfd31e
Benjamin Dewey <benjamin.m.dewey@gmail.com>
2018-05-13 19:53:09 -0400

    try data copy

--- Finetuning_clean.py
+++ Finetuning_clean.py
@@ -199,9 +199,7 @@ for param in myModel.parameters(): param.requires_grad = False
 copyModel = deepcopy(myModel)
 
 myModel.conv4_3_norm_gender    = nn.Conv2d(512, 2, kernel_size=3, stride=1, padding=1) # gender layer
-a = torch.Tensor(copyModel.conv4_3_norm_mbox_conf.weight[0])
-a = torch.cat((a, a), 0)
-myModel.conv4_3_norm_gender.weight = a
+myModel.conv4_3_norm_gender.weight[0].data.copy_(copyModel.conv4_3_norm_mbox_conf.weight[0].data)
 
 myModel.conv5_3_norm_gender    = nn.Conv2d(512, 2, kernel_size=3, stride=1, padding=1) # gender layer
 myModel.conv5_3_norm_gender.weight[0] = copyModel.conv5_3_norm_mbox_conf.weight[0]
