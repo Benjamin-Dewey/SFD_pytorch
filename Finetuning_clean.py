b3522ec91fd047a9c8b3052b52ed4249b612157d
Benjamin Dewey <benjamin.m.dewey@gmail.com>
2018-05-12 20:02:36 -0400

    test

--- Finetuning_clean.py
+++ Finetuning_clean.py
@@ -25,8 +25,8 @@ import os,sys,cv2,random,datetime,time,math
 import argparse
 import numpy as np
 
-from net_s3fd import *
-# from s3fd import *
+# from net_s3fd import *
+from s3fd import *
 from bbox import *
 from sklearn.preprocessing import MultiLabelBinarizer
 from PIL import Image
@@ -137,10 +137,15 @@ def train_model(model, criterion, optimizer, num_classes, num_epochs = 100):
 
 
             optimizer.zero_grad()
-            outputs = model(data)
-            print('--TYPE--')
-            print(type(outputs))
-            loss = criterion(outputs, target)
+            olist = model(data)
+
+            print('MADE IT THIS FAR - 1')
+
+            for i in range(len(olist)//2): olist[i*2] = F.softmax(olist[i*2])
+
+            print('MADE IT THIS FAR - 2')
+
+            loss = criterion(olist, target)
 
             if i%50==0:
                 print("Reached iteration ",i)
@@ -158,7 +163,7 @@ def train_model(model, criterion, optimizer, num_classes, num_epochs = 100):
 
 
 num_classes = 2
-myModel = s3fd(num_classes)
+myModel = s3fd_original()
 
 
 loadedModel = torch.load('s3fd_convert.pth')
