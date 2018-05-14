301c213aa08935c01bac54a1694be0d8d65c572d
Benjamin Dewey <benjamin.m.dewey@gmail.com>
2018-05-13 22:16:13 -0400

    train 150 epochs

--- Finetuning_clean.py
+++ Finetuning_clean.py
@@ -119,7 +119,7 @@ def save(model, optimizer, loss, filename):
 # In[6]:
 
 
-def train_model(model, criterion, optimizer, num_classes, num_epochs = 100):
+def train_model(model, criterion, optimizer, num_classes, num_epochs = 150):
     for epoch in range(num_epochs):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
